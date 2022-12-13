16d47c146e95a675b77d449388cb554068231044
gao <gao@qq.com>
2022-12-13 14:39:41 +0800

    two

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -58,16 +58,18 @@ import { validUsername } from '@/utils/validate'
 export default {
   name: 'Login',
   data() {
+    // 对用户名进行验证 只验证长度
     const validateUsername = (rule, value, callback) => {
-      if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
+      if (value.length < 5) {
+        callback(new Error('用户名长度不能小于5'))
       } else {
         callback()
       }
     }
+    // 对密码进行验证
     const validatePassword = (rule, value, callback) => {
       if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
+        callback(new Error('密码长度不能小于6'))
       } else {
         callback()
       }
